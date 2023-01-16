2fc96c73050df600c6b43ed17f270b17dbbf069a
popo63301 <popo63301@gmail.com>
2023-01-16 18:29:16 +0100

    notes

--- exos/exo1.js
+++ exos/exo1.js
@@ -3,15 +3,21 @@ const searchWord = (sentence, word) => {
   word = word.toLowerCase();
 
   const separator = [" ", ",", ";", ".", "!", "?"];
-  let isWord = false;
+  let isWord = false; // permettra de savoir si nous sommes dans un mot ou pas
   let currentWord = "";
   let firstCharacterPosition = null;
 
   for (let i = 0; i < sentence.length; i++) {
     const currentChar = sentence[i];
 
+    // si charactère est un séparateur de mot (espace, virgule, point, etc)
+    // alors, c'est la fin d'un mot
     if (separator.includes(currentChar)) {
       isWord = false;
+
+      // si c'est la fin d'un mot,
+      // soit on aura déjà accumuler le mot qu'on cherche
+      // soit, on va continuer
       if (currentWord === word) {
         return firstCharacterPosition;
       } else {
@@ -19,7 +25,11 @@ const searchWord = (sentence, word) => {
         continue;
       }
     } else {
+      // si le charactère n'est pas un séparateur,
+      // alors, nous sommes dans un mot
+      // donc, on va accumuler le charactère actuel pour constituer le mot
       if (isWord === false) {
+        // cas où on passe à un autre mot, donc avant on était pas dans un mot
         isWord = true;
         currentWord += currentChar;
         firstCharacterPosition = i;
@@ -29,7 +39,6 @@ const searchWord = (sentence, word) => {
     }
   }
   return null;
-  //   console.log(res);
 };
 
 export default searchWord;
--- exos/exo3_server/index.js
+++ exos/exo3_server/index.js
@@ -15,14 +15,7 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-app.get("/sum", async (req, res) => {
-  const sum = await User.aggregate([
-    { $group: { _id: null, totalAmount: { $sum: "$amount" } } },
-  ]);
-  console.log("sum", sum);
-  res.send("Sum is " + sum[0].totalAmount);
-});
-
+// 1) génère les datas (10 utilisateurs, nom + amount) avec cette requête: http://localhost:4000/generate_datas
 app.get("/generate_datas", async (req, res) => {
   console.log("ici");
   await User.remove({});
@@ -38,6 +31,7 @@ app.get("/generate_datas", async (req, res) => {
   res.send("Hello World!");
 });
 
+// 2) Voir les datas avec cette requête: http://localhost:4000/generate_datas
 app.get("/all_datas", async (req, res) => {
   const users = await User.find();
   console.log("users", users);
@@ -46,6 +40,15 @@ app.get("/all_datas", async (req, res) => {
     users.reduce((acc, currentValue) => acc + currentValue.amount, 0)
   );
   res.send("Hello World!");
+});
+
+// 3) Aggrège les datas
+app.get("/sum", async (req, res) => {
+  const sum = await User.aggregate([
+    { $group: { _id: null, totalAmount: { $sum: "$amount" } } },
+  ]);
+  console.log("sum", sum);
+  res.send("Sum is " + sum[0].totalAmount);
 });
 
 app.listen(port, () => {
